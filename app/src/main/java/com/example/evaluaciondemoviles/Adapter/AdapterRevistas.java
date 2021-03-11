876ce9507656352d99bdbead4da2487461a613b2
Msuarezl <74911743+Msuarezl@users.noreply.github.com>
2021-03-11 18:52:17 -0500

    Listado de Revistas

---
+++ app/src/main/java/com/example/evaluaciondemoviles/Adapter/AdapterRevistas.java
@@ -0,0 +1,36 @@
+package com.example.evaluaciondemoviles.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.evaluaciondemoviles.R;
+import com.example.evaluaciondemoviles.Revistas;
+
+import java.util.ArrayList;
+
+public class AdapterRevistas extends ArrayAdapter<Revistas> {
+
+    public AdapterRevistas(Context context, ArrayList<Revistas> datos) {
+        super(context, R.layout.layout_listart, datos);
+
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.layout_listart, null);
+        TextView Name, description;
+        ImageView img;
+        Name = item.findViewById(R.id.idTitulo);
+        description= item.findViewById(R.id.idcontenido);
+        img = item.findViewById(R.id.imaPortada);
+        Name.setText(getItem(position).getName());
+        description.setText(getItem(position).getDescripccion());
+        Glide.with(getContext()).load(getItem(position).getPortada()).into(img);
+        return(item);
+    }
+}
--- app/src/main/java/com/example/evaluaciondemoviles/MainActivity.java
+++ app/src/main/java/com/example/evaluaciondemoviles/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.evaluaciondemoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.evaluaciondemoviles.Adapter.AdapterRevistas;
+import com.example.evaluaciondemoviles.WebServices.Asynchtask;
+import com.example.evaluaciondemoviles.WebServices.WebService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity  implements Asynchtask {
+
+    ArrayList<Revistas> revistasList = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.idListRevistas);
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://revistas.uteq.edu.ec/ws/journals.php", datos, this, this);
+        ws.execute("");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+
+        JSONArray revistasJS=  new JSONArray(result);
+        for (int i = 0; i<revistasJS.length();i++)
+        {
+            Revistas revistas = new Revistas();
+            revistas.setName(revistasJS.getJSONObject(i).getString("name"));
+            revistas.setJournal_id(revistasJS.getJSONObject(i).getString("journal_id"));
+            revistas.setDescripccion(revistasJS.getJSONObject(i).getString("description"));
+            revistas.setAbbreviation(revistasJS.getJSONObject(i).getString("abbreviation"));
+            revistas.setJournalThumbnail(revistasJS.getJSONObject(i).getString("journalThumbnail"));
+            revistas.setPortada(revistasJS.getJSONObject(i).getString("portada"));
+            revistasList.add(revistas);
+        }
+        AdapterRevistas adapterBusiness = new AdapterRevistas(MainActivity.this, (ArrayList<Revistas>) revistasList);
+        listView.setAdapter(adapterBusiness);
+
+
     }
 }
